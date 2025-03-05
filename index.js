99b5c55204018e7f1e2519c376971ad96e887650
maheshmuttintidev <maheshmuttintidev@gmail.com>
2025-03-06 01:00:41 +0530

    add user and tasks apis

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const connectDB = require('./config/db')
+
+
+const authRoutes = require('./routes/auth');
+const taskRoutes = require('./routes/tasks')
+
+
+// Connect to the mongodb database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+// Routes
+app.use('/auth', authRoutes);
+app.use('/tasks', taskRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
